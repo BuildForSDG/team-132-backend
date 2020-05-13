5c6925730865875a2cd22e9828c5e182e6125273
francis-mwas <mwangif184@gmail.com>
2020-05-13 10:29:13 +0300

    Ch changed db config

---
+++ config/dbConfig.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// connect local db for local development
+const DB = process.env.DATABASE_LOCAL_URL;
+
+const dbConnect = () => {
+  mongoose
+    .connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log('DB connection successful!');
+    })
+    .catch((err) => {
+      console.log('an erro occurred:  ', err);
+    });
+};
+
+export default dbConnect;
--- server.js
+++ server.js
@@ -1,28 +1,5 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import app from './app';
+import startServer from './config/startServer';
 
-dotenv.config();
+// start the server
 
-// connect local db for local development
-const DB = process.env.DATABASE_LOCAL_URL;
-
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log('DB connection successful!');
-  })
-  .catch((err) => {
-    console.log('an erro occurred:  ', err);
-  });
-
-// START THE SERVER
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+startServer();
