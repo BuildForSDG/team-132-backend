6340fe026a13fbf39c329bbc168829e2641b5584
francis-mwas <mwangif184@gmail.com>
2020-05-26 00:01:56 +0300

    Added level product description

--- src/utils/helper/ussdHelper.js
+++ src/utils/helper/ussdHelper.js
@@ -1,16 +1,12 @@
 import User from '../../models/ussdUserModel';
 
 export const checkUser = async (phoneNumber) => {
-  try {
-    const user = await User.findOne({ phoneNumber });
-    // eslint-disable-next-line max-len
-    if (user && user.fullName !== undefined && user.email !== undefined && user.idNumber !== undefined) {
-      return user;
-    }
-    return undefined;
-  } catch (err) {
-    return undefined;
+  const user = await User.findOne({ phoneNumber });
+  // eslint-disable-next-line max-len
+  if (user && user.fullName !== undefined && user.email !== undefined && user.idNumber !== undefined) {
+    return user;
   }
+  return undefined;
 };
 
 export const ussdLevels = {
@@ -20,10 +16,6 @@ export const ussdLevels = {
   upload3: 'upload3',
   upload4: 'upload4',
   upload5: 'upload5',
-  farmInput1: 'farmInput1',
-  farmInput2: 'farmInput2',
-  farmInput3: 'farmInput3',
-  farmInput4: 'farmInput4',
   userRegister0: 'userRegister0',
   userRegister1: 'userRegister1',
   userRegister2: 'userRegister2',
