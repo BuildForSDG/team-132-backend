3c5baf020336276e571d4b47f51018d6a8544d5e
francis-mwas <mwangif184@gmail.com>
2020-06-07 22:39:05 +0300

    feat:(role based access) Authenticate users based on their role in the system The user role is passed in the user payload when logging in.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,15 +1,43 @@
-import { jwt } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
-export const auth = (req, res, next) => {
-  const token = req.header('x-auth-token');
+dotenv.config();
+
+// Check token
+// eslint-disable-next-line consistent-return
+export default (req, res, next) => {
+  // Get token from req.headers,authorization
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    // eslint-disable-next-line prefer-destructuring
+    token = req.headers.authorization.split(' ')[1];
+  } else {
+    return res.status(401).json({
+      status: false,
+      error: 'Unauthorised request'
+    });
+  }
+
+  // If there is no token, then the requst is unauthorised\
   if (!token) {
-    return res.status(401).json({ msg: 'No token authorization denied' });
+    return res.status(401).json({
+      status: false,
+      error: 'Unauthorised request'
+    });
   }
-  try {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    req.user = decoded.user;
-    return next();
-  } catch (error) {
-    return res.status(401).json({ msg: 'token is invalid' });
-  }
+
+  // Decode the token
+  // eslint-disable-next-line consistent-return
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    // Show an error if any when trying to decode the token
+    if (err) {
+      return res.status(403).json({
+        status: false,
+        error: `token expired, login again: ${err}`
+      });
+    }
+    // If the token was successfully decoded, assign the payload to req.user
+    req.user = decoded;
+    next();
+  });
 };
