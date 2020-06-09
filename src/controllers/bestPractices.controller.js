efcbf1ac2edf5e225406d37e1c012c50088f130d
obsessedyouth <52631736+obsessedyouth@users.noreply.github.com>
2020-06-09 11:08:13 +0100

    fix: body parameter

--- src/controllers/bestPractices.controller.js
+++ src/controllers/bestPractices.controller.js
@@ -3,7 +3,7 @@ import BestPractices from '../models/bestPractices';
 const BestPractice = BestPractices;
 
 export default function createBestPractice(req, res, next) {
-  const agroTest = new BestPractice({ ...req.body });
+  const agroTest = new BestPractice({ ...req.params });
   agroTest.save();
   res.json({ success: 'action posted' });
   next();
