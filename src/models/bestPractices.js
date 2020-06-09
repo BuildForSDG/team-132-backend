b0246548048aa3c4e8304b997d64bb08b721d153
francis-mwas <mwangif184@gmail.com>
2020-06-09 16:09:24 +0300

    fx:(add farming information) Allow agro chemical companies to write about best practices in organic farmin

--- src/models/bestPractices.js
+++ src/models/bestPractices.js
@@ -3,11 +3,30 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const bestPracticesSchema = new Schema({
-  title: String,
-  category: [String, String],
-  author: String,
-  company: String,
-  seeds: [String, String, String, String]
+  title: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  company: {
+    type: String,
+    required: true
+  },
+  seeds: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
 });
 
 export default mongoose.model('BestPractices', bestPracticesSchema);
