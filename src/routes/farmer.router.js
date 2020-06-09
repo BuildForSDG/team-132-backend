36cf2087f92937e125ad1259ab6ee2de06fae3d7
Denno31 <Denno31>
2020-06-09 07:37:06 +0300

    get farm produce

--- src/routes/farmer.router.js
+++ src/routes/farmer.router.js
@@ -4,11 +4,13 @@ import UssdController from '../controllers/ussdFarmerController';
 import FarmInputController from '../controllers/farmInput.controller';
 import isAuth from '../middleware/auth';
 import accessControl from '../middleware/access';
+import FarmProduct from '../controllers/farmProduct.controller';
 
 const router = Router();
 const { register, getAll, signIn } = FarmerController;
 const { postInput, updateFarmInput, getAllFarmInputs } = FarmInputController;
 const { getSingleFarmInput } = FarmInputController;
+const { getAllFarmProducts, getSingleFarmProduct } = FarmProduct;
 router.post('/register', register);
 router.post('/sign-in', signIn);
 const { registerFarmer } = UssdController;
@@ -18,5 +20,7 @@ router.post('/farm-input', isAuth, accessControl.restrictAccessTo('agro-chemical
 router.put('/farm-input/:id', isAuth, accessControl.restrictAccessTo('agro-chemical-company'), updateFarmInput);
 router.get('/farm-input/', isAuth, getAllFarmInputs);
 router.get('/farm-input/:id', isAuth, getSingleFarmInput);
+router.get('/farm-product', isAuth, getAllFarmProducts);
+router.get('/farm-product/:id', isAuth, getSingleFarmProduct);
 
 export default router;
